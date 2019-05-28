146e874135e0c3c7789ec0f9bf1d7c6a3c5c282c
Jordan Rocha <70rd23@gmail.com>
2019-05-28 11:54:13 -0300

    feat: draw mined board on mais screen

--- App.js
+++ App.js
@@ -3,10 +3,30 @@ import { StyleSheet, Text, View } from 'react-native'
 
 import params from './src/params'
 
-import Field from './src/components/Filed'
+import MinedField from './src/components/MineField'
+import { createMinedBoard } from './src/functions'
 
 export default class App extends Component {
-  render () {
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount())
+    }
+  }
+
+  render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Iniciando o Mines!</Text>
@@ -15,17 +35,9 @@ export default class App extends Component {
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
 
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={6} />
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened exploded />
-        <Field flagged />
-        <Field flagged opened />
+        <View style={styles.board}>
+          <MinedField board={this.state.board} />
+        </View>
       </View>
     )
   }
@@ -34,18 +46,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end'
+  },
+  board: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+    backgroundColor: '#aaa'
   }
 })
--- src/params.js
+++ src/params.js
@@ -1,4 +1,4 @@
-import { Dimensions } from "react-native";
+import { Dimensions } from 'react-native'
 
 const params = {
   blockSize: 30,
@@ -6,15 +6,15 @@ const params = {
   fontSize: 15,
   headerRatio: 0.15, // Proporção do painel superior da tela
   difficultLevel: 0.1, // Nível de dificuldade baseado no numero de campos
-  getColumnsAmount() {
-    const width = Dimensions.get("window").width;
-    return Math.floor(width / this.blockSize);
+  getColumnsAmount () {
+    const width = Dimensions.get('window').width
+    return Math.floor(width / this.blockSize)
   },
-  getRowsAmount() {
-    const totalHeight = Dimensions.get("window").height;
-    const boardHeight = totalHeight * (1 - this.headerRatio);
-    return Math.floor(boardHeight / this.blockSize);
+  getRowsAmount () {
+    const totalHeight = Dimensions.get('window').height
+    const boardHeight = totalHeight * (1 - this.headerRatio)
+    return Math.floor(boardHeight / this.blockSize)
   }
-};
+}
 
-export default params;
+export default params
